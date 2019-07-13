31dd5b48db8d5c20b11d4767088b337eb1e77889
lucasrct <lucasribeiro@live.com>
2019-07-13 10:58:31 -0300

    1. Adicionei mais funções ao IBGE.py. As funções contidas são:
    cod_to_mun(cod): dado um código de município (no formato dos dados do Flávio), rretorna o município referente.
    
    cor_to_uf(cod): mesmo do anterior, mas retornando a UF.
    
    return_loc(cod, municipio = True): retorna uma lista [lat,lng] com a latitude e longitude do município referente ao código. Se municipio = False, retorna a latitude e longitude da UF (fornecida pelo Google API).
    
    save_obj(obj,nome): salva um objeto do python em um arquivo no formato nome.pkl
    
    load_obj(nome): carrega um objeto em python contido no arquivo nome.pkl
    
    2. Como o Google API cobra por acesso e também é relativamente lento para retornar a latitude e longitude, criei um dicionario contendo {codigo_municpio: [lat,lng]} com todos os códigos que aparecem no dataframe do Flávio e salvei no arquivo dicionario_lat_lon_cod.pkl, se precisar, basta usar a função load_obj(dicionario_lat_lon_cod). Note que a função já retorna o objeto desejado.

--- IBGE.py
+++ IBGE.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import googlemaps
+import pickle
+
+gmaps = googlemaps.Client(key = 'AIzaSyBP1wd8YMxm9r5PTuI1kC7VbO8aMgmbbUk')
 
 def cod_to_mun(cod, completo=True):
     #dado um código, retorna o município referente
@@ -12,6 +16,8 @@ def cod_to_mun(cod, completo=True):
     if completo:
         return(df.loc[df[df['Código Município Completo'] == cod].index.item(),'Nome_Município'])
     return(df.loc[df[df['Município'] == cod].index.item(),'Nome_Município'])
+
+
 def cod_to_uf(cod):
     #dado um código, retorna a UF referente
     #cod = código do município
@@ -21,3 +27,28 @@ def cod_to_uf(cod):
             cod = x
             break    
     return(df.loc[df[df['Código Município Completo'] == cod].index.item(),'Nome_UF'])
+
+def return_loc(cod, municipio = True):
+    #se municipio = True, dado um código, retorna a latitude e longitude do município referente ao código
+    #se municipio = False, retorna a latitude referente a UF do código
+    #cod = codigo do município
+    #retorna: list com [lat,long]
+    if municipio:
+        latlng = gmaps.geocode(str(cod_to_mun(cod)) + ' ' + str(cod_to_uf(cod)) + ' Brazil')[0]['geometry']['location']
+        return(latlng[0],latlng[1])
+    latlng = gmaps.geocode(str(code_to_uf(cod)) + ' Brazil')[0]['geometry']['location']
+    return(latlng[0],latlng[1])
+
+def save_obj(obj, name):
+    #salva um objeto em python (lista, dict, df etc) em um arquivo no tipo .pkl
+    #entrada: obj = objeto a ser salvo , name = endereço/nome do arquivo a ser criado (sem o .pkl)
+    #saida: objeto na pasta
+    with open( name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(name):
+    #carrega um objeto em formato .pkl
+    #name: endedeço do arquivo .pkl (sem o .pkl)
+    #saida: o objeto guardado no arquivo .pkl
+    with open( name + '.pkl', 'rb') as f:
+        return pickle.load(f)
