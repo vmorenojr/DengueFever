505c9fd2a55ce4b5af9415544741a39224c7cc14
Valter Moreno <valter.moreno@eng.uerj.br>
2019-09-06 22:45:43 -0300

    New datasets

--- gen_datasets.py
+++ gen_datasets.py
@@ -49,8 +49,8 @@ def window(start, years=3):
 def window_data(df, target_date, window_length):
     most_recent = dt.strptime(target_date, '%Y-%m-%d') - timedelta(days=7)
     most_recent = most_recent.strftime('%Y-%m-%d')
-    start_date = window(most_recent, window_length)
-    return slice_date(df, start_date, most_recent)
+    earliest_date = window(most_recent, window_length)
+    return slice_date(df, earliest_date, most_recent)
 
 # Get distances between cities
 def get_distances(df, city):
@@ -61,39 +61,66 @@ def gen_dataset(df, target_city,
                 target_start_date, 
                 target_years_training = 5, 
                 window_years = 3):
-        
+    
     target_dates = slice_date(df['dt_sintoma'].to_frame(), 
                             window(target_start_date, target_years_training),
                             target_start_date)
     target_dates = target_dates['dt_sintoma'].unique()
     target_dates = target_dates[:(len(target_dates)-1)]
 
+    ocorr_alvo = df.loc[(df['municipio'] == target_city) &
+                        (df['dt_sintoma'] == target_start_date), 'ocorrencias'].values[0]   
+    
+    por_hab_alvo = df.loc[(df['municipio'] == target_city) &
+                          (df['dt_sintoma'] == target_start_date), 'por_habitante'].values[0] 
+    
     dados_modelo = window_data(df, target_start_date, window_years)
-    dados_modelo.insert(0,'capital',target_city)
-    dados_modelo.insert(1,'data_alvo',target_start_date)
-    dados_modelo.insert(dados_modelo.shape[1],'distancia',0)
-
+    dados_modelo.insert(0, 'capital', target_city)
+    dados_modelo.insert(1, 'data_alvo', target_start_date)
+    dados_modelo.insert(2, 'ocorrencias_alvo', ocorr_alvo)
+    dados_modelo.insert(3, 'por_habitante_alvo', por_hab_alvo)
+    
+    lags = pd.to_datetime(dados_modelo['dt_sintoma'], format='%Y-%m-%d')
+    lags = lags.apply(lambda x: (dt.strptime(target_start_date, '%Y-%m-%d')-x).days/7)
+    dados_modelo.insert(6, 'lag', lags)
+ 
+    dados_modelo.insert(dados_modelo.shape[1], 'distancia', 0)  
+  
     for date in target_dates:
+        ocorr_alvo = df.loc[(df['municipio'] == target_city) &
+                            (df['dt_sintoma'] == date), 'ocorrencias'].values[0]   
+    
+        por_hab_alvo = df.loc[(df['municipio'] == target_city) &
+                              (df['dt_sintoma'] == date), 'por_habitante'].values[0] 
+     
         dados_window = window_data(df, date, window_years)
         dados_window.insert(0, 'capital', target_city)
         dados_window.insert(1, 'data_alvo', date)
+        dados_window.insert(2, 'ocorrencias_alvo', ocorr_alvo)
+        dados_window.insert(3, 'por_habitante_alvo', por_hab_alvo)
         dados_window.insert(dados_window.shape[1], 'distancia',0)
+        
+        lags = pd.to_datetime(dados_window['dt_sintoma'], format='%Y-%m-%d')
+        lags = lags.apply(lambda x: (dt.strptime(date, '%Y-%m-%d')-x).days/7)
+        dados_window.insert(6, 'lag', lags)
+        
         dados_modelo = dados_modelo.append(dados_window)
 
     for city in dist['municipio']:
         valor = dist.loc[dist['municipio']==target_city, city].values[0]
-        dados_modelo.loc[dados_modelo['municipio']==city, 'distancia'] = valor
+        dados_modelo.loc[dados_modelo['municipio']==city, 'distancia'] = valor/100
 
     return dados_modelo
 
 # Create and save datasets
-for city in ['São Luís', 'São Paulo', 'Teresina', 'Vitória']:#municipios:
-    file_path = 'Dados/Datasets/' + city + '.csv.gz'
-    new_dataset = gen_dataset(dados[['dt_sintoma', 'municipio', 
-                                     'ocorrencias', 'por_habitante']], 
+for city in municipios:
+    fim = dados[dados['municipio']==city]['dt_sintoma'].max()
+    new_dataset = gen_dataset(dados[['municipio', 'dt_sintoma', 'ocorrencias', 
+                                     'por_habitante']], 
                               target_city=city,
                               target_start_date=fim,
                               target_years_training = 5, 
                               window_years = 3)
+    file_path = 'Dados/Datasets/' + city + '.csv.gz'
     new_dataset.to_csv(file_path, index=False)
 
